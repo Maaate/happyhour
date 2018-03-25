f36a7cace3ae4e5f57c410431867f09bd2ec3c0a
hoffbeam <hoff@beamwallet.com>
2018-03-25 18:19:55 +1100

    Make sure health check is generated from conf file

--- app/controllers/SystemController.scala
+++ app/controllers/SystemController.scala
@@ -4,18 +4,19 @@ import scala.concurrent.Future
 
 import javax.inject.Inject
 import models.HealthCheck
+import play.api.Configuration
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-class SystemController @Inject()(implicit override val messagesApi: MessagesApi, cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+class SystemController @Inject()(implicit override val messagesApi: MessagesApi, cc: ControllerComponents, config: Configuration) extends AbstractController(cc) with I18nSupport {
 
   import models.HealthCheckProtocol._
 
 
   def healthCheck() = Action.async {
     implicit request => {
-      Future.successful(Ok(Json.toJson(HealthCheck(0))))
+      Future.successful(Ok(Json.toJson(HealthCheck(config.get[Int]("external.appVersion.minimum")))))
     }
   }
 
