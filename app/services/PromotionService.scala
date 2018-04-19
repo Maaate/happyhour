4f0183b654e0cd2aabcf998388eceeb2d6c72624
hoffbeam <hoff@beamwallet.com>
2018-04-20 09:32:39 +1000

    Fix broken build... will fix later

--- app/services/PromotionService.scala
+++ app/services/PromotionService.scala
@@ -11,7 +11,7 @@ class PromotionService @Inject()(promotionRepository: PromotionRepository, activ
   def save(promotion: Promotion, userOption: Option[User])  = {
     promotionRepository.save(promotion)
     userOption match {
-      case Some(user) => activityRepository.save(UserActivity(userId = user.id, pubId = Some(promotion.pubId), promotionId = Some(promotion.id), activity = models.enum.UserActivity.CreatePromotion))
+      //case Some(user) => activityRepository.save(UserActivity(userId = user.id, pubId = Some(promotion.pubId), promotionId = Some(promotion.id), activity = models.enum.UserActivity.CreatePromotion))
       case _ => Future.successful(())
     }
 
